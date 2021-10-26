f5c1f4280515c882d709cecfcf7299f56c550c6d
Gustavo Martínez de Murga <gustavo.murga1996@gmail.com>
2021-10-26 15:29:52 -0400

    Remove the unused actions in reduce

--- src/reducer.js
+++ src/reducer.js
@@ -5,26 +5,14 @@ export const initialState = {
 }
 
 export const actionTypes = {
-    ADD_TO_BASKET: "ADD_TO_BASKET",
     SET_BASKET: "SET_BASKET",
     SET_TOKEN: "SET_TOKEN",
-    REMOVE_ITEM_FROM_BASKET: "REMOVE_ITEM_FROM_BASKET",
     SET_USER: "SET_USER",
-    EMPTY_BASKET: "EMPTY_BASKET",
-}
-
-export const getBasketTotal = (basket) => {
-    return basket?.reduce((ammount, item) => ammount + item.price, 0)
 }
 
 const reducer = (state, action) => {
     console.log(action)
     switch (action.type) {
-        case "ADD_TO_BASKET":
-            return {
-                ...state,
-                basket: [...state.basket, action.item]
-            }
         case "SET_TOKEN":
             return {
                 ...state,
@@ -35,30 +23,11 @@ const reducer = (state, action) => {
                 ...state,
                 basket: action.basket,
             }
-        case "REMOVE_ITEM_FROM_BASKET":
-            const index = state.basket.findIndex((item => item.id === action.id))
-            let newBasket = [...state.basket]
-            console.log(index)
-            console.log(action.id)
-            if( index >= 0 ){
-                newBasket.splice(index, 1)
-            }else {console.log("Cant remove product")}
-            return {
-                ...state,
-                basket: newBasket,
-            }
         case "SET_USER":  
         return {
                 ...state,
                 user: action.user
             }
-        case "EMPTY_BASKET":
-            return {
-                ...state,
-                basket: action.basket
-            }
-        default:
-            return state;
     }
 }
 
